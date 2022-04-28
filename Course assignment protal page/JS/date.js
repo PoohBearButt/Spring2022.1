bdae4ab532cc97d3afdfb3eb42584ead469b5a4e
PoohBearButt <elisha.w716@gmail.com>
2022-04-27 18:04:16 -0600

    portal page

---
+++ Course assignment protal page/JS/date.js
@@ -0,0 +1,13 @@
+/*var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();*/
+
+/*function dateFunction(){
+    document.getElementID("demo").innerHTML = "paragraph change";
+}*/
+
+
+let modifiedTime = new Date(document.lastModified);
+let timeDisplayed = `Last Updated: ${modifiedTime}`;
+
+document.querySelector("#lastUpdate").innerHTML = timeDisplayed;
+console.log(modifiedTime);
